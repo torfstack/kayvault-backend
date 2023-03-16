5a335abe3cbfdabaad0e0361132b7cb46f601687
torfstack <torfstack@gmail.com>
2023-03-16 22:25:49 +0100

    launch mariadb in cluster

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,8 @@ dependencies {
 
 	implementation("io.github.microutils:kotlin-logging:3.0.5")
 
-	runtimeOnly("com.h2database:h2:2.1.214")
+	//runtimeOnly("com.h2database:h2:2.1.214")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.1.2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")

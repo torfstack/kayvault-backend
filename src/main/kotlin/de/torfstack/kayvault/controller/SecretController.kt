765895524fd17407e67806a587fb154cb5597d4b
Max Torf <torfstack@gmail.com>
2023-06-25 22:50:13 +0200

    added: logging in SecretController

--- src/main/kotlin/de/torfstack/kayvault/controller/SecretController.kt
+++ src/main/kotlin/de/torfstack/kayvault/controller/SecretController.kt
@@ -1,18 +1,12 @@
 package de.torfstack.kayvault.controller
 
-import com.google.api.gax.rpc.InvalidArgumentException
-import com.nimbusds.jwt.SignedJWT
-import de.torfstack.kayvault.persistence.SecretEntity
 import de.torfstack.kayvault.persistence.SecretModel
 import de.torfstack.kayvault.persistence.SecretService
 import de.torfstack.kayvault.validation.TokenValidator
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RestController
-import java.lang.IllegalArgumentException
+import mu.KotlinLogging
+import org.springframework.web.bind.annotation.*
+
+val Log = KotlinLogging.logger{}
 
 @RestController
 @CrossOrigin
@@ -21,12 +15,14 @@ class SecretController(val secretService: SecretService, val tokenVerifier: Toke
     @GetMapping("secret")
     fun getSecret(@RequestHeader authorization: String): List<SecretRequestEntity> {
         val user = userFromHeader(authorization)
+        Log.info { "returning secrets for user $user" }
         return secretsForUser(user)
     }
 
     @PostMapping("secret")
     fun postSecret(@RequestHeader authorization: String, @RequestBody entity: SecretRequestEntity): List<SecretRequestEntity> {
         val user = userFromHeader(authorization)
+        Log.info { "adding a secret for user $user" }
         secretService.addSecretForUser(user, entity.toModel())
         return secretsForUser(user)
     }
@@ -34,8 +30,14 @@ class SecretController(val secretService: SecretService, val tokenVerifier: Toke
     private fun userFromHeader(header: String): String {
         val authorization = header.removePrefix("Bearer")
         when (val result = tokenVerifier.validate(authorization)) {
-            is TokenValidator.InvalidVerification -> throw IllegalArgumentException("token could not be verified", result.ex)
-            is TokenValidator.ValidVerification -> return result.user
+            is TokenValidator.InvalidVerification -> {
+                Log.warn { "token could not be verified" }
+                throw IllegalArgumentException("token could not be verified", result.ex)
+            }
+            is TokenValidator.ValidVerification -> {
+                Log.debug { "validated token successfully and got user ${result.user}" }
+                return result.user
+            }
         }
     }
 
@@ -49,7 +51,6 @@ class SecretController(val secretService: SecretService, val tokenVerifier: Toke
                     notes = ""
                 )
             }
-            .ifEmpty { emptyList() }
     }
 
     data class SecretRequestEntity(
